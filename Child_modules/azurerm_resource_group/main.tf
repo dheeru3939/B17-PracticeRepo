4f1edb47c45786fc618c010a6f7ee4ce25cdbb7b
unknown <www.dheerendraverma@gmail.com>
2025-07-24 21:30:36 +0530

    code added

--- Child_modules/azurerm_resource_group/main.tf
+++ Child_modules/azurerm_resource_group/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg1" {
-  name = var.resourece_group.name
-  location = var.resourece_group.location
+  name     = var.resourece_group_name
+  location = var.resourece_group_location
 }
