31e5e579c9918f79eda390900b36f3a3dda200db
unknown <www.dheerendraverma@gmail.com>
2025-08-01 05:23:34 +0530

    new rg created

--- Parent_modules_Infra/main.tf
+++ Parent_modules_Infra/main.tf
@@ -3,3 +3,9 @@ module "resourece_group" {
     resourece_group_name = "RG-dheeruinfra39"
     resourece_group_location = "Australia East"
 }
+
+module "resourece_group2" {
+    source = "../Child_modules/azurerm_resource_group"
+    resourece_group_name = "RG-Purchase"
+    resourece_group_location = "Australia East"
+}
